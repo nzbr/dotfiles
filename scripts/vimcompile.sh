9592510b0c5ab310223d0fb28208dc1f604e1da7
Nico Jansen <mail@nzbr.de>
2019-01-11 01:54:55 +0100

    add vimcompile.sh

---
+++ scripts/vimcompile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+temp=$(mktemp)
+echo "\$ $@" | tee $temp
+
+set -o pipefail
+"$@" |& tee /dev/stderr >>$temp
+code="$?"
+set +o pipefail
+if ! [ $code -eq 0 ]; then
+	less +G $temp
+fi
+
+rm $temp
