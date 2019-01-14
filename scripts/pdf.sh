2d210be9957141e8ec7eddbe6bc3f13c1fe7f037
nzbr <mail@nzbr.de>
2019-01-14 12:24:05 +0100

    Add filters to pdf.sh

--- scripts/pdf.sh
+++ scripts/pdf.sh
@@ -3,8 +3,20 @@ TEMPLATEURL=https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template
 TEMPLATEDIR=~/.pandoc/templates
 TEMPLATE=eisvogel
 TEMPLATEFILE=$TEMPLATEDIR/$TEMPLATE.latex
+
 if ! [ -f ~/.pandoc/templates/eisvogel.latex ]; then
 	mkdir -p $TEMPLATEDIR
 	wget $TEMPLATEURL -O $TEMPLATEFILE
 fi
-pandoc --template $TEMPLATE -o "${1%.*}.pdf" "$1"
+
+# pandoc
+# texlive-full
+# pandoc-include-code (AUR)
+# pandoc-plantuml-filter-git (PIP3)
+
+pandoc \
+	--template $TEMPLATE \
+	--filter pandoc-plantuml \
+	--filter pandoc-include-code \
+	-o "${1%.*}.pdf" \
+	"$1"
