8ed2224c00671cf0201ebf11d5c51a6d4922f168
nzbr <mail@nzbr.de>
2020-07-21 20:05:21 +0200

    Add migration script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,16 @@
-echo "MIGRATION SCRIPT!"
+alias dotgit 'command git --git-dir=$HOME/.git-hidden --work-tree=$HOME'
+
+echo "Migrating dotfiles..."
+set temp (mktemp)
+cp -v control.sh "$temp"
+
+set files (dotgit ls-tree --full-tree --name-only -r HEAD)
+rm -vrf $files
+for file in $files
+	rmdir (dirname $file)
+end
+rm -rf .git-hidden
+
+bash "$temp"
+exec fish
+
