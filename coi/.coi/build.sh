d64d0352690efe2a99435d23002ee1b45fd5b6fd
Wire Jansen <mail@nzbr.de>
2026-09-08 10:03:48 +0200

    coi: move .claude-shared link cretion to after dotfiles installation

--- coi/.coi/build.sh
+++ coi/.coi/build.sh
@@ -92,14 +92,6 @@ chmod +x /etc/coi/set-environment.sh
 
 ln -s /etc/coi/set-environment.sh /etc/profile.d/20-coi.sh
 
-# Shared Claude Code History, Skills and Styles
-sudo -u code mkdir -p /home/code/.claude{,-shared/{projects,skills,output-styles}}
-rm -rf /home/code/.claude/{projects,skills,output-styles}
-ln -sfn /home/code/.claude-shared/projects /home/code/.claude/projects
-ln -sfn /home/code/.claude-shared/skills /home/code/.claude/skills
-ln -sfn /home/code/.claude-shared/output-styles /home/code/.claude/output-styles
-chown -h 1000:1000 /home/code/.claude/{projects,skills,output-styles}
-
 # Claude notification relay
 cat >/etc/tmpfiles.d/run-user-1000.conf <<EOF
 d /run/user/1000 0700 1000 1000 - -
@@ -170,6 +162,14 @@ if [ -f "$statusline/Cargo.toml" ]; then
 	apt-get purge -y cargo
 	apt-get autoremove -y --purge
 fi
+
+# Shared Claude Code History, Skills and Styles
+sudo -u code mkdir -p /home/code/.claude{,-shared/{projects,skills,output-styles}}
+rm -rf /home/code/.claude/{projects,skills,output-styles}
+ln -sfn /home/code/.claude-shared/projects /home/code/.claude/projects
+ln -sfn /home/code/.claude-shared/skills /home/code/.claude/skills
+ln -sfn /home/code/.claude-shared/output-styles /home/code/.claude/output-styles
+chown -h 1000:1000 /home/code/.claude/{projects,skills,output-styles}
 
 apt-get clean
 rm -rf /var/lib/apt/lists/*
